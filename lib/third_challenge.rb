89f5efb8951d4641930bea8850314e7bec29b281
Reese M Bowman <reesemb1@gmail.com>
2019-12-17 01:18:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,15 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"}
+      :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "54", status: "alive"}
+      :hero => {name: "Romeo", age: "54", status: "alive"},
       :hero_friends => []
     }, 
    :capulet => {
-      :patriarch => 
-      :matriarch => 
-      :heroine => 
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
