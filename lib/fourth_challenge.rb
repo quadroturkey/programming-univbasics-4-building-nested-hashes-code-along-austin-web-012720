d846d66243145f9fc47d7df555ea615b43aed7fd
Reese M Bowman <reesemb1@gmail.com>
2019-12-17 01:24:54 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        :Benvolio => {name: "Benvolio", age: "17", status: "worried"},
+        :Mercutio => {name: "Mercutio", age: "18", status: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -13,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        :steven => {name: "Steven", age: "30", status: "confused"},
+        :nurse => {name: "Nurse", age: "44", status: "worried"}
       ]
    }
 }
